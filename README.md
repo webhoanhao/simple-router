eafcb2604229bfdb0e3a5b28891fed7d45495562
WebHoanHao <webhoanhao@gmail.com>
2021-03-29 21:56:40 +0700

    init package

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# simple-router
-This is a very simple and small PHP router for your url routing in your project.
+# SimpleRouter
+A very simple and small PHP router for url routing in your project.
+
+## Simple example:
+```php
+// code in: ./public/index.php
+
+// Require the router class
+include '../src/Route.php';
+
+// Require the controller class (in your project)
+include '../app/Controllers/HomeController.php';
+
+// Use router namespace
+use WebHoanHao\SimpleRouter\Route;
+
+// Use app namespace (in your project)
+use App\Controllers\HomeController;
+
+// Add your routes
+Route::add('/', HomeController::class,'index','index-route-name');
+Route::add('/hello', HomeController::class,'hello','hello-route-name');
+Route::add('/product-detail', ProductController::class,'detail','product-detail');
+// ....
+
+// Run the router
+Route::run();
+```
+
+## Example with Composer  
+For installation, just run 
+`composer require webhoanhao/simple-router`
+
+Than add the autoloader to your project like this:
+```php
+// code in: ./public/index.php
+
+// Autoload files using composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Use router namespace
+use WebHoanHao\SimpleRouter\Route;
+
+// Use app namespace (in your project)
+use App\Controllers\HomeController;
+
+// Add your routes
+Route::add('/', HomeController::class,'index','index-route-name');
+Route::add('/hello', HomeController::class,'hello','hello-route-name');
+Route::add('/product-detail', ProductController::class,'detail','product-detail');
+// ....
+
+// Run the router
+Route::run();
+```
+
+## Get URL from route BY NAME
+```php
+$url = Route::url('routeName');
+```
+If there are some params
+```php
+$url = Route::url('routeName',[$param_1,2,'param 3']);
+```
+
+## Get params in target method
+
+```php
+    public function detail($param1, $param2, $param3)
+    {
+        echo "Param1 = ".$param1;
+        echo "Param2 = ".$param2;
+        echo "Param3 = ".$param3;
+    }
+```
+
+## License
+This project is licensed under the MIT License. See LICENSE for further information.
---
+++ src/Route.php
@@ -0,0 +1,105 @@
+<?php
+
+
+namespace WebHoanHao\SimpleRouter;
+
+
+class Route
+{
+    private static $routes = [];
+
+    public static function add($route, $controller, $method, $name)
+    {
+        self::$routes[] = Array($name,trim($route),$controller,$method,[]);
+    }
+
+    private static function resolve($pathString)
+    {
+        $matched = false;
+        $route = null;
+        foreach (self::$routes as $item) {
+            if (!empty($item[1]) && strpos($pathString, $item[1]) === 0) {
+                $matched = true;
+                $route = $item;
+                break;
+            }
+        }
+
+        if (!$matched) {
+            throw new \Exception('Could not match route.');
+        }
+
+        if (trim($route[1]) !== '/') {
+            $paramStr = str_replace($route[1], '', $pathString);
+        } else {
+            $paramStr = $pathString;
+        }
+        $params = explode('/', trim($paramStr, '/'));
+        $params = array_filter($params);
+
+        $route[4] = $params;
+        return $route;
+    }
+
+    public static function run()
+    {
+        $request = $_SERVER['REQUEST_URI'];
+        try {
+            $match = self::resolve($request);
+            if (is_array($match) && count($match)>0) {
+                list($name,$route,$controller,$method,$params) = $match;
+                try {
+                    $reflectedMethod = new \ReflectionMethod($controller, $method);
+                    if ($reflectedMethod->isPublic() && (!$reflectedMethod->isAbstract())) {
+                        if ($reflectedMethod->isStatic()) {
+                            forward_static_call_array(array($controller, $method), $params);
+                        } else {
+                            if (is_string($controller)) {
+                                $controller = new $controller();
+                            }
+                            call_user_func_array(array($controller, $method), $params);
+                        }
+                    }
+                } catch (\ReflectionException $reflectionException) {
+                    echo $reflectionException->getMessage();
+                }
+            }
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public static function url($routeName, $params = [], $basePath = '')
+    {
+        if (trim($routeName) === '') {
+            return self::baseUrl();
+        }
+        $url = self::baseUrl($basePath);
+        foreach (self::$routes as $route) {
+            if (is_array($route) && $route[0] === $routeName) {
+                $url .= '/'.trim($route[1],'/');
+                break;
+            }
+        }
+        if (is_array($params) && count($params)) {
+            $url .= '/'.implode('/',$params);
+        }
+        return $url;
+    }
+
+    private static function baseUrl($basePath = '') {
+        $pageUrl = 'http';
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+            $pageUrl .= "s";
+        }
+        $pageUrl .= "://";
+        $pageUrl .= $_SERVER["SERVER_NAME"];
+        if ($_SERVER["SERVER_PORT"] !== '80') {
+            $pageUrl .= ":" . $_SERVER["SERVER_PORT"];
+        }
+        if (trim($basePath, '/') !== '') {
+            $pageUrl .= trim($basePath, '/');
+        }
+        return $pageUrl;
+    }
+}
